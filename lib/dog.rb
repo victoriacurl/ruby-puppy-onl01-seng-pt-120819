e22ecde3e32d1df1086aadc19b363b0daf63d91e
Victoria Curl <victoriacurl9@gmail.com>
2020-02-12 21:05:27 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,31 @@
-# Add your code here
+
+class Dog
+
+attr_accessor :name
+
+  @@all = []
+
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+def self.all
+  @@all
+    end
+
+def self.clear_all
+  @@all.clear
+end
+
+def self.print_all
+  @@all.each do |d|
+    puts d.name
+    end
+  end
+
+def save (dog)
+  @@all << self
+end
+ end
